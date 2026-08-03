90e8ae25c63723540887e201e5191b7fca66f9de
Tanseychuong <nyangchuongtiutiu@gmail.com>
2026-08-03 20:17:15 +0000

    __init__.py changed

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -1,1 +1,5 @@
 """Persistence repository package."""
+
+from app.repositories import course_repository, material_repository, user_repository
+
+__all__ = ["course_repository", "material_repository", "user_repository"]
