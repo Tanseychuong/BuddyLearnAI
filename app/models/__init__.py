9e82db22bd7913ec73305145436d2b91350e37be
Tanseychuong <nyangchuongtiutiu@gmail.com>
2026-08-03 20:07:56 +0000

    __init__.py changed

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,7 @@
 """Database model package."""
+
+from app.models.course import Course
+from app.models.material import Material, MaterialStatus
+from app.models.user import User
+
+__all__ = ["Course", "Material", "MaterialStatus", "User"]
